ca0fc3e70b406333bff31e2afe4b4d280cf2086d
Humberto Alves Mesquita <humbertoalvesti@gmail.com>
2025-01-19 18:03:56 -0300

    Test Activity 8

--- submission/008.sh
+++ submission/008.sh
@@ -3,6 +3,6 @@
 
 txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
 transaction=$(bitcoin-cli getrawtransaction $txid 1)
-key=$(echo $transaction | jq -r .vin[0].txinwitness[2])
+key=$(echo $transaction | jq -r '.vin[0].txinwitness[2]' | cut -c5-70)
 
 echo $key
