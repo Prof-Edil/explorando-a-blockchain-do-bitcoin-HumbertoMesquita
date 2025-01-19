0d1b4e0a6275d140d355e6d89b4e56f6beb69217
Humberto Alves Mesquita <humbertoalvesti@gmail.com>
2025-01-19 17:59:46 -0300

    Test Activity 8

--- submission/008.sh
+++ submission/008.sh
@@ -1,9 +1,11 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 
+#!/bin/bash
+
 txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
 transaction=$(bitcoin-cli getrawtransaction "$txid" 1)
-key=$(echo "$transaction" | jq -r '.vin[0].txinwitness[1]')
+key=$(echo "$transaction" | jq -r '.vin[0].txinwitness[2]')
 
 echo "$key"
 
