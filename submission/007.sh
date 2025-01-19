314ffdbf0c3f8214d13a161058f3fefbbd35b7ad
Humberto Alves Mesquita <humbertoalvesti@gmail.com>
2025-01-19 17:53:37 -0300

    Test Activity 7

--- submission/007.sh
+++ submission/007.sh
@@ -1,7 +1,7 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
 
 api="https://blockchain.info/block-height/123321?format=json"
-data=$(curl -s "api")
+data=$(curl -s "$api")
 output=$(echo "$data" | jq -r '.blocks[0].tx[] | .out[] | select(.spent == false) | .addr' | head -n 1)
 
 echo "$output"
