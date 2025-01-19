4be7272536ed766f67e65ebe655f122790220491
Humberto Alves Mesquita <humbertoalvesti@gmail.com>
2025-01-19 16:17:13 -0300

    Test Activity 1

--- submission/001.sh
+++ submission/001.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#
 # What is the hash of block 654,321?
 
-bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_237 -rpcpassword=4ZxquYgsVAlI getblockhash 654321
+bitcoin-cli getblockhash 654321
+
 
